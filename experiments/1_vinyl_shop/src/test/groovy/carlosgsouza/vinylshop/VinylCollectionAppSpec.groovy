b309603f0ddf1523600a707f6bece544cb7bcb31
carlos souza <carlosgsouza@gmail.com>
2013-10-31 18:47:19 -0200

    Adding search and list functionalities to songs, year and genre

--- experiments/1_vinyl_shop/src/test/groovy/carlosgsouza/vinylshop/VinylCollectionAppSpec.groovy
+++ experiments/1_vinyl_shop/src/test/groovy/carlosgsouza/vinylshop/VinylCollectionAppSpec.groovy
@@ -5,7 +5,10 @@ import carlosgsouza.derails.Console
 import carlosgsouza.derails.Form
 import carlosgsouza.derails.View
 import carlosgsouza.vinylshop.controller.ArtistController
+import carlosgsouza.vinylshop.controller.GenreController;
+import carlosgsouza.vinylshop.controller.SongController;
 import carlosgsouza.vinylshop.controller.VinylController
+import carlosgsouza.vinylshop.controller.YearController;
 import carlosgsouza.vinylshop.model.Vinyl
 import carlosgsouza.vinylshop.view.UiFactory
 
@@ -13,6 +16,10 @@ class VinylCollectionAppSpec extends Specification {
 	
 	VinylController vinylController
 	ArtistController artistController
+	YearController yearController
+	GenreController genreController
+	SongController songController 
+	
 	UiFactory uiFactory
 	Console console
 	
@@ -36,10 +43,15 @@ class VinylCollectionAppSpec extends Specification {
 		
 		vinylController = Mock(VinylController)
 		artistController = Mock(ArtistController)
+		yearController = Mock(YearController)
+		genreController = Mock(GenreController)
+		songController = Mock(SongController)
+		
+		
 		uiFactory = Mock(UiFactory)
 		console = Mock(Console)
 		
-		app = new VinylCollectionApp(artistController: artistController, vinylController:vinylController, uiFactory:uiFactory, console:console)
+		app = new VinylCollectionApp(yearController:yearController, genreController:genreController, songController:songController, artistController: artistController, vinylController:vinylController, uiFactory:uiFactory, console:console)
 	}
 	
 	def "should create a bunch of vinyl during bootstrap"() {
@@ -101,19 +113,19 @@ class VinylCollectionAppSpec extends Specification {
 		
 		then:
 		1 * vinylController.search("Album Title") >> [vinylA, vinylB]
-		1 * uiFactory.searchVinyls("Album Title", [vinylA, vinylB]) >> view
+		1 * uiFactory.searchByTitle("Album Title", [vinylA, vinylB]) >> view
 		1 * console.render(view)
 	}
 	
-//	def "should search for an artist"() {
-//		when:
-//		app.routeRequest("artist", "search", "Artist Name")
-//		
-//		then:
-//		1 * artistController.search("Artist Name") >> ["Art", vinylB]
-//		1 * uiFactory.searchArtist("Artist Name", [vinylA, vinylB]) >> view
-//		1 * console.render(view)
-//	}
+	def "should search for an artist"() {
+		when:
+		app.routeRequest("artist", "search", "Art")
+		
+		then:
+		1 * artistController.search("Art") >> [vinylA, vinylB]
+		1 * uiFactory.searchByArtist("Art", [vinylA, vinylB]) >> view
+		1 * console.render(view)
+	}
 	
 	def "should list all artists"() {
 		when:
@@ -122,6 +134,66 @@ class VinylCollectionAppSpec extends Specification {
 		then:
 		1 * artistController.list() >> ["Artist 1", "Artist 2"]
 		1 * uiFactory.listArtists(["Artist 1", "Artist 2"]) >> view
+		1 * console.render(view)
+	}
+	
+	def "should search for an genre"() {
+		when:
+		app.routeRequest("genre", "search", "Art")
+		
+		then:
+		1 * genreController.search("Art") >> [vinylA, vinylB]
+		1 * uiFactory.searchByGenre("Art", [vinylA, vinylB]) >> view
+		1 * console.render(view)
+	}
+	
+	def "should list all genres"() {
+		when:
+		app.routeRequest("genre", "list", null)
+		
+		then:
+		1 * genreController.list() >> ["Genre 1", "Genre 2"]
+		1 * uiFactory.listGenres(["Genre 1", "Genre 2"]) >> view
+		1 * console.render(view)
+	}
+	
+	def "should search for an year"() {
+		when:
+		app.routeRequest("year", "search", "Art")
+		
+		then:
+		1 * yearController.search("Art") >> [vinylA, vinylB]
+		1 * uiFactory.searchByYear("Art", [vinylA, vinylB]) >> view
+		1 * console.render(view)
+	}
+	
+	def "should list all years"() {
+		when:
+		app.routeRequest("year", "list", null)
+		
+		then:
+		1 * yearController.list() >> ["Year 1", "Year 2"]
+		1 * uiFactory.listYears(["Year 1", "Year 2"]) >> view
+		1 * console.render(view)
+	}
+	
+	def "should search for an song"() {
+		when:
+		app.routeRequest("song", "search", "Art")
+		
+		then:
+		1 * songController.search("Art") >> [vinylA, vinylB]
+		1 * uiFactory.searchBySong("Art", [vinylA, vinylB]) >> view
+		1 * console.render(view)
+	}
+	
+	def "should list all songs"() {
+		when:
+		app.routeRequest("song", "list", null)
+		
+		then:
+		1 * songController.list() >> ["Song 1", "Song 2"]
+		1 * uiFactory.listSongs(["Song 1", "Song 2"]) >> view
 		1 * console.render(view)
 	}
 	
