1c94594296b72af1582868fcf2033ee160577dce
carlos souza <carlosgsouza@gmail.com>
2013-12-17 14:25:01 -0200

    Adding files for 1a2

---
+++ experiments/1_vinyl_collection/a2/typed/src/main/groovy/carlosgsouza/derails/Form.groovy
@@ -0,0 +1,8 @@
+package carlosgsouza.derails;
+
+
+public class Form {
+	public String title
+	public Map<String, String> fields = new HashMap<String, String>();
+	public List<String> fieldName = new ArrayList<String>();
+}
